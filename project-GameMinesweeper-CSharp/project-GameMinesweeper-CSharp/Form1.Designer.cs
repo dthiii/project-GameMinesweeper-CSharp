68a7fc1643e508f9d614c963cdb1881f4ad99fc9
Nguyen Tran Doan Thi <thinguyentran123@gmail.com>
2025-04-15 12:43:29 +0700

    project-GameMinesweeper-CSharp

---
+++ project-GameMinesweeper-CSharp/project-GameMinesweeper-CSharp/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace project_GameMinesweeper_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBody = new System.Windows.Forms.Panel();
+            this.pnlInfor = new System.Windows.Forms.Panel();
+            this.lbScore = new System.Windows.Forms.Label();
+            this.lbFlag = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtFlag = new System.Windows.Forms.TextBox();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.levelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlInfor.SuspendLayout();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBody
+            // 
+            this.pnlBody.Location = new System.Drawing.Point(0, 75);
+            this.pnlBody.Name = "pnlBody";
+            this.pnlBody.Size = new System.Drawing.Size(308, 259);
+            this.pnlBody.TabIndex = 13;
+            // 
+            // pnlInfor
+            // 
+            this.pnlInfor.Controls.Add(this.lbScore);
+            this.pnlInfor.Controls.Add(this.lbFlag);
+            this.pnlInfor.Controls.Add(this.txtScore);
+            this.pnlInfor.Controls.Add(this.txtFlag);
+            this.pnlInfor.Location = new System.Drawing.Point(0, 28);
+            this.pnlInfor.Name = "pnlInfor";
+            this.pnlInfor.Padding = new System.Windows.Forms.Padding(4);
+            this.pnlInfor.Size = new System.Drawing.Size(308, 46);
+            this.pnlInfor.TabIndex = 14;
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbScore.Location = new System.Drawing.Point(155, 20);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(72, 22);
+            this.lbScore.TabIndex = 7;
+            this.lbScore.Text = "Score:";
+            // 
+            // lbFlag
+            // 
+            this.lbFlag.AutoSize = true;
+            this.lbFlag.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFlag.Location = new System.Drawing.Point(11, 20);
+            this.lbFlag.Name = "lbFlag";
+            this.lbFlag.Size = new System.Drawing.Size(59, 22);
+            this.lbFlag.TabIndex = 6;
+            this.lbFlag.Text = "Flag:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.Red;
+            this.txtScore.Location = new System.Drawing.Point(233, 7);
+            this.txtScore.Multiline = true;
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(64, 34);
+            this.txtScore.TabIndex = 11;
+            this.txtScore.TabStop = false;
+            this.txtScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtFlag
+            // 
+            this.txtFlag.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFlag.ForeColor = System.Drawing.Color.Red;
+            this.txtFlag.Location = new System.Drawing.Point(72, 7);
+            this.txtFlag.Multiline = true;
+            this.txtFlag.Name = "txtFlag";
+            this.txtFlag.Size = new System.Drawing.Size(60, 34);
+            this.txtFlag.TabIndex = 9;
+            this.txtFlag.TabStop = false;
+            this.txtFlag.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.levelToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(309, 28);
+            this.menuStrip.TabIndex = 15;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // levelToolStripMenuItem
+            // 
+            this.levelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.levelToolStripMenuItem.Name = "levelToolStripMenuItem";
+            this.levelToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.levelToolStripMenuItem.Text = "Level";
+            this.levelToolStripMenuItem.Click += new System.EventHandler(this.levelToolStripMenuItem_Click);
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.easyToolStripMenuItem.Text = "Easy";
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(147, 26);
+            this.hardToolStripMenuItem.Text = "Hard";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 346);
+            this.Controls.Add(this.pnlInfor);
+            this.Controls.Add(this.pnlBody);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Minesweeper";
+            this.pnlInfor.ResumeLayout(false);
+            this.pnlInfor.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlBody;
+        private System.Windows.Forms.Panel pnlInfor;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtFlag;
+        private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.Label lbFlag;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem levelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ project-GameMinesweeper-CSharp/project-GameMinesweeper-CSharp/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_GameMinesweeper_CSharp
+{
+    public partial class Form1 : Form
+    {
+        //0 là chưa gán giá trị
+        //10 là vị trí có mìn
+        //1 đến 8 là vị trí số
+        //20 là vị trí trống
+
+        byte[,] Positions;
+        Button[,] ButtonList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            easyToolStripMenuItem.Click += new EventHandler(levelToolStripMenuItem_Click);
+            mediumToolStripMenuItem.Click += new EventHandler(levelToolStripMenuItem_Click);
+            hardToolStripMenuItem.Click += new EventHandler(levelToolStripMenuItem_Click);
+        }
+
+        private void levelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem clickedItem = sender as ToolStripMenuItem;
+            int row = 0, col = 0, mine = 0, flag = 0;
+
+            if (clickedItem.Text == "Easy")
+            {
+                row = 9;
+                col = 9;
+                mine = 10;
+                pnlBody.Size = new Size(308, 259);
+                this.ClientSize = new Size(232, 270);
+            }
+            else if (clickedItem.Text == "Medium")
+            {
+                row = 16;
+                col = 16;
+                mine = 40;
+                pnlBody.Size = new Size(544, 442);
+                this.ClientSize = new Size(405, 425);
+            }
+            else if (clickedItem.Text == "Hard")
+            {
+                row = 16;
+                col = 30;
+                mine = 99;
+                pnlBody.Size = new Size(1008, 442);
+                this.ClientSize = new Size(760, 425);
+            }
+
+            pnlBody.Controls.Clear();
+            txtFlag.Clear();
+            txtScore.Clear();
+
+            points = 0;
+            flag = mine;
+            txtFlag.Text = mine.ToString();
+            Positions = new byte[row, col];
+            ButtonList = new Button[row, col];
+            pnlBody.Enabled = true;
+            GenerateMines(row, col, mine);
+            GeneratePositionValue(row, col);
+            GenerateButtons(row, col);
+        }
+
+        private void GenerateMines(int row, int col, int mine)
+        {
+            Random rnd = new Random();
+            int CountMines = 0;
+            while (CountMines < mine)
+            {
+                int x = rnd.Next(0, row);
+                int y = rnd.Next(0, col);
+
+                if (Positions[x, y] == 0)
+                {
+                    Positions[x, y] = 10;
+                    CountMines++;
+                }
+            }
+        }
+
+        private void GeneratePositionValue(int row, int col)
+        {
+            for (int x = 0; x < row; x++)
+            {
+                for (int y = 0; y < col; y++)
+                {
+                    if (Positions[x, y] == 10)
+                        continue;
+
+                    byte mineCounts = 0;
+                    for (int counterX = -1; counterX < 2; counterX++)
+                    {
+                        int checkerX = x + counterX;
+
+                        for (int counterY = -1; counterY < 2; counterY++)
+                        {
+                            int checkerY = y + counterY;
+
+                            if (checkerX == -1 || checkerY == -1 || checkerX > row - 1 || checkerY > col - 1)
+                                continue;
+
+                            if (checkerX == x && checkerY == y)
+                                continue;
+
+                            if (Positions[checkerX, checkerY] == 10)
+                                mineCounts++;
+                        }
+                    }
+
+                    if (mineCounts == 0)
+                        Positions[x, y] = 20;
+                    else
+                        Positions[x, y] = mineCounts;
+                }
+            }
+        }
+
+        private void GenerateButtons(int row, int col)
+        {
+            int xLoc = 3;
+            int yLoc = 6;
+
+            for (int x = 0; x < row; x++)
+            {
+                for (int y = 0; y < col; y++)
+                {
+                    Button btn = new Button();
+                    btn.Parent = pnlBody;
+                    btn.Location = new Point(xLoc, yLoc);
+                    btn.Size = new Size(25, 22);
+                    btn.Tag = $"{x},{y}";
+                    btn.Click += BtnClick;
+                    btn.MouseUp += BtnMouseUp;
+                    xLoc += 25;
+                    ButtonList[x, y] = btn;
+                }
+                yLoc += 22;
+                xLoc = 3;
+            }
+        }
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+            byte value = Positions[x, y];
+
+            if (value == 10)
+            {
+                btn.Image = Nhom8_LaptrinhGameMinesweeper_9301.Properties.Resources.mine;
+                ShowAllMines();
+                MessageBox.Show("GameOver");
+                pnlBody.Enabled = false;
+            }
+            else if (value == 20)
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                btn.FlatAppearance.BorderSize = 1;
+                btn.Enabled = false;
+                OpenAdjacentEmptyTile(btn);
+                points++;
+            }
+            else
+            {
+                btn.FlatStyle = FlatStyle.Flat;
+                btn.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                btn.FlatAppearance.MouseDownBackColor = btn.BackColor;
+                btn.FlatAppearance.MouseOverBackColor = btn.BackColor;
+                btn.Text = Positions[x, y].ToString();
+                points++;
+            }
+
+            btn.Click -= BtnClick;
+            txtScore.Text = points.ToString();
+
+            int mineCount = 0;
+            foreach (int i in Positions)
+            {
+                if (i == 10)
+                    mineCount++;
+            }
+
+            if (points == (Positions.GetLength(0) * Positions.GetLength(1) - mineCount))
+            {
+                ShowAllMines();
+                MessageBox.Show("You win");
+                pnlBody.Enabled = false;
+            }
+        }
+
+        private void OpenAdjacentEmptyTile(Button btn)
+        {
+            int x = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(0));
+            int y = Convert.ToInt32(btn.Tag.ToString().Split(',').GetValue(1));
+            List<Button> emptyButtons = new List<Button>();
+
+            for (int counterX = -1; counterX < 2; counterX++)
+            {
+                int checkerX = x + counterX;
+
+                for (int counterY = -1; counterY < 2; counterY++)
+                {
+                    int checkerY = y + counterY;
+
+                    if (checkerX == -1 || checkerY == -1 || checkerX > Positions.GetLength(0) - 1 || checkerY > Positions.GetLength(1) - 1)
+                        continue;
+
+                    if (checkerX == x && checkerY == y)
+                        continue;
+
+                    Button btnAdjacent = ButtonList[checkerX, checkerY];
+                    byte value = Positions[checkerX, checkerY];
+
+                    if (value == 20)
+                    {
+                        if (btnAdjacent.FlatStyle != FlatStyle.Flat)
+                        {
+
+                            btnAdjacent.FlatStyle = FlatStyle.Flat;
+                            btnAdjacent.FlatAppearance.BorderSize = 1;
+                            btnAdjacent.FlatAppearance.BorderColor = SystemColors.ControlDark;
+                            btnAdjacent.Enabled = false;
+                            emptyButtons.Add(btnAdjacent);
+                            points++;
+                        }
+                    }
+
+                    else if (value != 10)
+                        btnAdjacent.PerformClick();
+
+                    else if (btn.Enabled == false && btn.Image != null)
+                    {
+                        btn.Image = null; // Xóa lá cờ
+                        txtFlag.Text = (Convert.ToInt32(txtFlag.Text) + 1).ToString();
+                    }
+
+                }
+            }
+
+            foreach (var btnEmpty in emptyButtons)
+            {
+                OpenAdjacentEmptyTile(btnEmpty);
+            }
+
+            txtScore.Text = points.ToString();
+        }
+
+        private void ShowAllMines()
+        {
+            for (int x = 0; x < Positions.GetLength(0); x++)
+            {
+                for (int y = 0; y < Positions.GetLength(1); y++)
+                {
+                    if (Positions[x, y] == 10)
+                        ButtonList[x, y].Image = Nhom8_LaptrinhGameMinesweeper_9301.Properties.Resources.mine;
+                }
+            }
+        }
+
+        int points = 0;
+        private void BtnMouseUp(object sender, MouseEventArgs e)
+        {
+            int flag = Convert.ToInt32(txtFlag.Text);
+            if (e.Button == MouseButtons.Right)
+            {
+                Button btn = (Button)sender;
+                if (btn.Image == null && btn.Text == "" && flag > 0)
+                {
+                    btn.Image = Nhom8_LaptrinhGameMinesweeper_9301.Properties.Resources.flag;
+                    btn.Click -= BtnClick;
+                    flag--;
+                }
+                else if (btn.Image != null)
+                {
+                    btn.Image = null;
+                    btn.Click += BtnClick;
+                    flag++;
+                }
+                txtFlag.Text = flag.ToString();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
